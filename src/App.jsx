9a4e8f197a55a4b53a107f5a04241db145c2f699
penso996 <penso.andrea@outlook.it>
2025-06-20 14:34:03 +0200

    snack 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import useSwitch from "./useSwitch";
+import useDate from "./useDate";
 
 function App() {
+
   const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
-    <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
-    </div>
+    <>
+      <div>
+        <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
+
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
+      </div>
+    </>
   );
 }
 
---
+++ src/useDate.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+export default function useDate() {
+
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return currentDate;
+}
